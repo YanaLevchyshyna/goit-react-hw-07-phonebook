6af500c2239c32f46a89fb3342c4b8f16e8f05e1
Yana-Levchyshyna <levchyshynayana@gmail.com>
2023-12-14 01:18:40 +0200

    changed mockapi url

--- src/fetchContacts/fetchContacts.js
+++ src/fetchContacts/fetchContacts.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://65118ba8829fa0248e404ffa.mockapi.io/api/v1/';
+axios.defaults.baseURL = 'https://656870759927836bd974cfd0.mockapi.io';
 
 export async function fetchContacts() {
   const { data } = await axios.get('/contacts');
